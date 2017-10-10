38f033ff13b5c4678e1d70049729eb4086060b62
Dylan Maxwell <maxwelld@frib.msu.edu>
2017-10-10 17:15:45 -0400

    Remove reference to es6-promise types, promise types from tsconfig instead

--- src/web/ts/index.d.ts
+++ src/web/ts/index.d.ts
@@ -2,4 +2,3 @@
  * All external type declarations should be referenced here.
  */
 /// <reference path="../../app/webapi.d.ts" />
-/// <reference path="../../../node_modules/es6-promise/es6-promise.d.ts" />
