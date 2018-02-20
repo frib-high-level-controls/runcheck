4d2ed03ca41acb42f67ffe04caa2b87054fb7197
Dylan Maxwell <maxwelld@frib.msu.edu>
2018-02-20 16:16:56 -0500

    Merge testing utilities from master branch

--- src/apptest/route-groups-test.ts
+++ src/apptest/route-groups-test.ts
@@ -6,9 +6,7 @@
 
 import { AssertionError } from 'assert';
 
-import { assert } from 'chai';
 import * as express from 'express';
-import * as request from 'supertest';
 
 import { Group } from '../app/models/group';
 import { Slot } from '../app/models/slot';
@@ -16,17 +14,11 @@ import { Slot } from '../app/models/slot';
 import * as app from './app';
 import * as data from './data';
 
-// Utility to get an authenticated agent for the specified user.
-async function requestFor(app: express.Application, username?: string, password?: string) {
-  const agent = request.agent(app);
-  if (username) {
-  await agent
-    .get('/login')
-    .auth(username, password || 'Pa5w0rd')
-    .expect(302);
-  }
-  return agent;
-};
+import {
+  expectPackage,
+  requestFor,
+} from './shared/testing';
+
 
 async function getGroupId(r: { group: string }) {
   let group = await Group.findOne({ name: r.group }).exec();
@@ -44,28 +36,6 @@ async function getSlotId(r: { name: string }) {
   return slot.id;
 }
 
-function expectPackage(data?: {}) {
-  return (res: request.Response) => {
-  if (res.status < 300 || res.status >= 400) {
-    let pkg = <webapi.Pkg<{}>> res.body;
-    assert.isObject(pkg);
-    if (res.status < 300) {
-    assert.isObject(pkg.data);
-    assert.isUndefined(pkg.error);
-    if (data) {
-      // For some reason this function, deepInclude(),
-      // is not in the type definitions (@types/chai@4.0.5)!
-      (<any> assert).deepInclude(pkg.data, data);
-    }
-    } else {
-      assert.isObject(pkg.error);
-      assert.isNumber((<any> pkg.error).code);
-      assert.isString((<any> pkg.error).message);
-      assert.isNotEmpty((<any> pkg.error).message);
-      }
-  }
-  };
-}
 
 describe('Test group routes', () => {
   let handler: express.Application;
